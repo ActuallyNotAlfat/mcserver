94aaad250a70d3a02cd5937bfc1f2d484cc4fce0
ActuallyNotAlfat <thediamondcraft77@gmail.com>
2023-04-09 08:00:00 +0000

    Update!

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const cron = require('node-cron');
 const { exec } = require("child_process");
 
-exec("./backup.sh", (error, stdout, stderr) => {
-  if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-  }
-  if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-  }
-  console.log(`stdout: ${stdout}`);
-});
+//exec("./backup.sh", (error, stdout, stderr) => {
+//  if (error) {
+//      console.log(`error: ${error.message}`);
+//      return;
+//  }
+//  if (stderr) {
+//      console.log(`stderr: ${stderr}`);
+//      return;
+//  }
+//  console.log(`stdout: ${stdout}`);
+//});
 
-//cron.schedule("0 */4 * * *", () => {
-//  exec("./backup.sh", (error, stdout, stderr) => {
-//    if (error) {
-//        console.log(`error: ${error.message}`);
-//        return;
-//    }
-//    if (stderr) {
-//        console.log(`stderr: ${stderr}`);
-//        return;
-//    }
-//    console.log(`stdout: ${stdout}`);
-//  });
-//})
+cron.schedule("0 */4 * * *", () => {
+  exec("./backup.sh", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+})
