1b0bf3d8724f6db564bfbbdc7c36ea1af9544a38
ActuallyNotAlfat <thediamondcraft77@gmail.com>
2023-04-09 07:10:09 +0000

    Update!

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
 const cron = require('node-cron');
 const { exec } = require("child_process");
 
-cron.schedule("0 */4 * * *", () => {
-  exec("git add .", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-    exec("git commit -am 'Save'", (error, stdout, stderr) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-      if (stderr) {
-          console.log(`stderr: ${stderr}`);
-          return;
-      }
-      console.log(`stdout: ${stdout}`);
-      exec("git push -u https://$GH_ACCESS@github.com/ActuallyNotAlfat/mcserver.git main", (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-      });
-    });
-  });
-})
+exec("./backup.sh", (error, stdout, stderr) => {
+  if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+  }
+  if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
+
+//cron.schedule("0 */4 * * *", () => {
+//  exec("./backup.sh", (error, stdout, stderr) => {
+//    if (error) {
+//        console.log(`error: ${error.message}`);
+//        return;
+//    }
+//    if (stderr) {
+//        console.log(`stderr: ${stderr}`);
+//        return;
+//    }
+//    console.log(`stdout: ${stdout}`);
+//  });
+//})
